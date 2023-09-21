6e814e4ddd6ec072cabe02b8faccd02659fb483d
Ricardo <ricarditomontserrat@gmail.com>
2023-09-21 02:45:13 +0200

    It works for most of it, only spaces missing

--- index.ts
+++ index.ts
@@ -3,45 +3,20 @@ import { log } from "console";
 class ClassDecl {
 	constructor(public name: string, public methods: MethodDecl[]) {}
 }
-
 class MethodDecl {
 	constructor(public name: string, public type: string | null, public body: string) {}
 }
 
 class Visitor {
 	visitClassDecl(decl: ClassDecl) {
-		let ctx = "function " + decl.name + "() {}\n";
+		let ctx = `function ${decl.name}() {}\n`;
 		decl.methods.forEach((method) => {
-			ctx += decl.name + (method.type !== "static" ? ".prototype." : ".") + method.name + " = function() {\n";
-			ctx += method.body + "\n";
-			ctx += "}\n";
+			const methodName = `${decl.name}${method.type !== "static" ? ".prototype." : "."}${method.name}`;
+			ctx += `${methodName} = function() { ${method.body} }\n`;
 		});
 		return ctx;
 	}
 }
-
-const bookClassDecl: ClassDecl = {
-	name: "Book",
-	methods: [
-		{
-			name: "addBook",
-			type: null,
-			body: "console.log('addBook');",
-		},
-		{
-			name: "removeBook",
-			type: null,
-			body: "console.log('removeBook');",
-		},
-		{
-			name: "getBook",
-			type: "static",
-			body: "return 'getBook';",
-		},
-	],
-};
-
-// log(new Visitor().visitClassDecl(bookClassDecl));
 
 enum Type {
 	NUM = "NUMBER",
@@ -56,6 +31,17 @@ enum Type {
 	EOF = "EOF",
 }
 
+enum Keyword {
+	CLASS = "class",
+	FUNCTION = "function",
+	PROTOTYPE = "prototype",
+	STATIC = "static",
+	LET = "let",
+	VAR = "var",
+	TYPEOF = "typeof",
+	CONST = "const",
+}
+
 type Token = { type: Type; value?: string | number };
 
 // Also known as tokenizer, lexer
@@ -64,103 +50,117 @@ class Scanner {
 		let current = "";
 		let tokens: Token[] = [];
 
+		function saveToken(type: Type, value?: string | number) {
+			tokens.push(value ? { type, value } : { type }); // To remove the value:undefined print
+			current = "";
+		}
+
 		for (let i = 0; i < text.length; i++) {
 			current += text[i];
 			current = current.trim();
 
 			const next = text[i + 1];
-
 			if (current === "") continue;
 
 			if ([Type.LBRACE, Type.RBRACE, Type.LPAR, Type.RPAR].includes(current as Type)) {
-				tokens.push({ type: current as Type });
-				current = "";
+				saveToken(current as Type);
 				continue;
 			}
 
 			if (Number(current) && !Number(next)) {
-				tokens.push({ type: Type.NUM, value: Number(current) });
-				current = "";
+				saveToken(Type.NUM, Number(current));
 				continue;
 			}
 
 			const operators = "+-/*%>=!";
 			if (operators.includes(current) && !operators.includes(next)) {
-				tokens.push({ type: Type.OP, value: current });
-				current = "";
+				saveToken(Type.OP, current);
 				continue;
 			}
 
-			const isAlphanumeric = (value: string) => /\w/.exec(value);
+			const isText = (value: string) => /[\w"']/.exec(value);
 
-			if (isAlphanumeric(current) && !isAlphanumeric(next)) {
-				const keywords = "class function prototype static var typeof";
-				const isKeyword = keywords.includes(current);
-				tokens.push({ type: isKeyword ? Type.KWORD : Type.IDENT, value: current });
-				current = "";
+			if (isText(current) && !isText(next)) {
+				const keywords = Object.values(Keyword) as string[];
+				saveToken(keywords.includes(current) ? Type.KWORD : Type.IDENT, current);
 				continue;
 			}
 
 			if ([";", "\n"].includes(current)) {
-				tokens.push({ type: Type.EOL, value: current });
-				current = "";
+				saveToken(Type.EOL, current);
 				continue;
 			}
 		}
 
-		tokens.push({ type: Type.EOF, value: "" });
-
+		saveToken(Type.EOF);
 		return tokens;
 	}
 }
 
+enum Context {
+	CLASS = "class",
+	METHOD = "method",
+	// TODO: Use this context for spaces
+	STRING = "string",
+	NONE = "none",
+}
+
 class Parser {
-	parse(tokens: Token[]): ClassDecl[] {
-		const ast: ClassDecl[] = [];
+	constructor(public tokens: Token[], public index = 0, public methods: MethodDecl[] = [], public ctx = Context.NONE) {}
 
-		for (let i = 0; i < tokens.length; i++) {
-			let currToken = tokens[i];
-
-			if (currToken.type === Type.KWORD && currToken.value === "class") {
-				const className = tokens[i + 1];
-				if (className.type === Type.IDENT) {
-					while (i < tokens.length && tokens[i].type !== Type.LBRACE) {
-						i++;
-					}
-					i++;
-					const methods: MethodDecl[] = [];
-					while (i < tokens.length && tokens[i].type !== Type.EOF) {
-						currToken = tokens[i];
-						let methodName = "";
-						let methodType: string | null = null;
-						let body = "";
-
-						if (currToken.type === Type.KWORD) {
-							methodType = currToken.value as string;
-							methodName = tokens[i + 1].value as string;
-							i += 2;
-						} else if (currToken.type === Type.IDENT) {
-							methodName = currToken.value as string;
-							i++;
-						}
-
-						while (i < tokens.length && tokens[i].type !== Type.LBRACE) {
-							i++;
-						}
-						i++;
-
-						while (i < tokens.length && tokens[i].type !== Type.RBRACE) {
-							body += tokens[i].value;
-							i++;
-						}
-
-						if (methodName) methods.push(new MethodDecl(methodName, methodType, body));
-						i++;
-					}
-					const classDecl = new ClassDecl(className.value + "", methods);
-					ast.push(classDecl);
-				}
+	openContext(context: Context) {
+		let currToken;
+		while ((currToken = this.tokens[this.index++])) {
+			if (currToken.type === Type.LBRACE) {
+				this.ctx = context;
+				break;
 			}
+		}
+	}
+
+	closeContext() {
+		if (this.ctx === Context.CLASS) this.ctx = Context.NONE;
+		else if (this.ctx === Context.METHOD) this.ctx = Context.CLASS;
+	}
+
+	parseKeyword(token: Token, ast: ClassDecl[]) {
+		const nextToken: Token = this.tokens[this.index++];
+		if (token.value === Context.CLASS && nextToken.type === Type.IDENT) {
+			ast.push(new ClassDecl(nextToken.value + "", (this.methods = [])));
+			this.openContext(Context.CLASS);
+		} else if (token.value === "static" && this.ctx === Context.CLASS && nextToken.type === Type.IDENT) {
+			this.openMethodContext(nextToken.value + "", "static");
+		}
+	}
+
+	parseIdentifier(token: Token) {
+		if (this.ctx === Context.CLASS) {
+			this.openMethodContext(token.value + "");
+		} else if (this.ctx === Context.METHOD) {
+			this.appendToLastMethodBody(token.value + "");
+		}
+	}
+
+	openMethodContext(name: string, type: string = "") {
+		this.methods.push(new MethodDecl(name, type, ""));
+		this.openContext(Context.METHOD);
+	}
+
+	appendToLastMethodBody(value: string | number) {
+		this.methods[this.methods.length - 1].body += value;
+	}
+
+	parse(): ClassDecl[] {
+		const ast: ClassDecl[] = [];
+		let currToken: Token | undefined;
+
+		while ((currToken = this.tokens[this.index++])) {
+			const tokType = currToken.type;
+
+			if (tokType === Type.KWORD) this.parseKeyword(currToken, ast);
+			else if (tokType === Type.IDENT) this.parseIdentifier(currToken);
+			else if (tokType === Type.RBRACE) this.closeContext();
+			else if (this.ctx === Context.METHOD) this.appendToLastMethodBody(currToken.value ?? currToken.type);
 		}
 
 		return ast;
@@ -169,15 +169,21 @@ class Parser {
 
 const code = `
 class Book {
-  addBook() { }
+  addBook() { 
+		var syke = "There is no book";
+		return syke;
+	}
   removeBook() { }
-  static getBook() { }
+  static getBook() { 
+		console.log("text"); 
+		println("This is Lol");
+	}
 }`;
 
 const tokens = new Scanner().tokenize(code);
 log("Tokens: ", tokens);
 
-const ast = new Parser().parse(tokens);
+const ast = new Parser(tokens).parse();
 log("AST: ", ast);
 
 log(new Visitor().visitClassDecl(ast[0]));
